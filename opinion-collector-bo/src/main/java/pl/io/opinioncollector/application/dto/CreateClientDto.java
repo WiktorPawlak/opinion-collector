25347f838aed48259152686e142d21908c8b1726
236665 Strzelecki Mateusz <236665@edu.p.lodz.pl>
2022-12-08 15:45:14 +0100

    Add authorization button for swagger

---
+++ opinion-collector-bo/src/main/java/pl/io/opinioncollector/application/dto/CreateClientDto.java
@@ -0,0 +1,25 @@
+//package pl.io.opinioncollector.application.dto;
+//
+//import lombok.AllArgsConstructor;
+//import pl.io.opinioncollector.domain.client.model.*;
+//
+//
+//@AllArgsConstructor
+//public class CreateClientDto {
+//    private String id;
+//    private String username;
+//    private String password;
+//    private String rePassword;
+//    private String email;
+//    private ClientRole role = ClientRole.STANDARD;
+//
+////    public Client toDomain() {
+////        return new Client(
+////            new ClientId(Long.parseLong(id)),
+////            new ClientUsername(username),
+////            new ClientEmail(email),
+////            type,
+////
+////        );
+////    }
+//}
