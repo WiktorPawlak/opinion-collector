77d13c023ac41d08d19fed8c0fb9b9a4eeef1346
Krayan321 <krayan321@gmail.com>
2022-12-11 22:11:18 +0100

    Added preauthorize

--- opinion-collector-bo/src/main/java/pl/io/opinioncollector/application/controllers/CategoryController.java
+++ opinion-collector-bo/src/main/java/pl/io/opinioncollector/application/controllers/CategoryController.java
@@ -1,6 +1,7 @@
 package pl.io.opinioncollector.application.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,30 +20,32 @@ import java.util.List;
 public class CategoryController {
     private final CategoryFacade categoryFacade;
 
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @GetMapping
     public List<Category> getAllCategories() {
         return categoryFacade.findAll();
     }
 
 
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @GetMapping("/{id}")
     public Category getCategory(@PathVariable long id) {
         return categoryFacade.get(id);
     }
 
-
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PostMapping
     public Category addCategory(@RequestBody Category category) {
         return categoryFacade.add(category);
     }
 
-
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PutMapping
     public void editCategory(@RequestBody Category category) {
         categoryFacade.edit(category);
     }
 
-
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PostMapping("/delete")
     public void deleteCategory(@PathVariable long id) {
         categoryFacade.delete(id);
