03f90d437b870d62d81493a31bcf3ddf512e0d03
Radek <236607@edu.p.lodz.pl>
2022-12-11 20:53:34 +0100

    Change password, change email

--- opinion-collector-bo/src/main/java/pl/io/opinioncollector/application/controllers/ClientController.java
+++ opinion-collector-bo/src/main/java/pl/io/opinioncollector/application/controllers/ClientController.java
@@ -1,15 +1,18 @@
 package pl.io.opinioncollector.application.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.io.opinioncollector.domain.client.ClientFacade;
 import pl.io.opinioncollector.domain.client.model.Client;
 
 import java.security.Principal;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,19 +20,41 @@ import java.security.Principal;
 public class ClientController {
 
     private final ClientFacade clientFacade;
-    @GetMapping("/self")
-    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
-    public Client getClient(Principal principal) {
-        String username = principal.getName();
 
-        return clientFacade.getClient(username);
+    @GetMapping("/self")
+    public ResponseEntity<Object> getClient(Principal principal) {
+        try {
+            return ResponseEntity.ok(clientFacade.getClient(principal.getName()));
+        } catch (IllegalStateException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
+
     }
 
+    @GetMapping
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public ResponseEntity<List<Client>> getClients() {
+        return ResponseEntity.ok(clientFacade.getAllClients());
+    }
 
-    @PostMapping("changeEmail")
-    public void changeEmail(String clientId, String email) {
+    @PutMapping("/change-email")
+    public ResponseEntity<Object> changeEmail(Principal principal, String email) {
+        try {
+            clientFacade.changeEmail(principal.getName(), email);
+            return ResponseEntity.ok("Email changed");
+        } catch (IllegalStateException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
+    }
 
-        clientFacade.changeEmail(clientId, email);
+    @PutMapping("/change-password")
+    public ResponseEntity<Object> changePass(Principal principal, String hashedPass) {
+        try {
+            clientFacade.changePass(principal.getName(), hashedPass);
+            return ResponseEntity.ok("Password changed");
+        } catch (IllegalStateException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
     }
 
 }
--- opinion-collector-bo/src/main/java/pl/io/opinioncollector/domain/client/model/ClientId.java
+++ opinion-collector-bo/src/main/java/pl/io/opinioncollector/domain/client/model/ClientId.java
@@ -2,20 +2,28 @@ package pl.io.opinioncollector.domain.client.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Embeddable;
+import javax.persistence.GeneratedValue;
 import java.io.Serializable;
 import java.util.UUID;
+@Getter
 @Data
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientId implements Serializable {
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+        name = "UUID",
+        strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    private UUID uuid ;
 
-    private UUID uuid = UUID.randomUUID();
 
-    public ClientId(String uuid) {
-        this.uuid = UUID.fromString(uuid);
-    }
 }
