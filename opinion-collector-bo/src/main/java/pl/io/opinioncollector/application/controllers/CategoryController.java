8392d4311260d7c75347293347c7a185f29563ba
Krayan321 <krayan321@gmail.com>
2022-12-11 19:09:29 +0100

    Fixed according to comments

---
+++ opinion-collector-bo/src/main/java/pl/io/opinioncollector/application/controllers/CategoryController.java
@@ -0,0 +1,47 @@
+package pl.io.opinioncollector.application.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.io.opinioncollector.domain.category.CategoryFacade;
+import pl.io.opinioncollector.domain.category.model.Category;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryFacade categoryFacade;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryFacade.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategory(@PathVariable long id) {
+        return categoryFacade.get(id);
+    }
+
+    @PostMapping
+    public Category addCategory(@RequestBody Category category) {
+        return categoryFacade.add(category);
+    }
+
+    @PutMapping
+    public void editCategory(@RequestBody Category category) {
+        categoryFacade.edit(category);
+    }
+
+    @PostMapping("/delete")
+    public void deleteCategory(@PathVariable long id) {
+        categoryFacade.delete(id);
+    }
+
+}
--- opinion-collector-bo/src/main/java/pl/io/opinioncollector/domain/category/model/Category.java
+++ opinion-collector-bo/src/main/java/pl/io/opinioncollector/domain/category/model/Category.java
@@ -1,7 +1,30 @@
 package pl.io.opinioncollector.domain.category.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Category")
 public class Category {
-    private CategoryId id;
-    private CategoryName name;
-    private Category parent;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categoryId", nullable = false)
+    private long categoryId;
+    private String parent = null;
+    private String categoryName = null;
+    @ElementCollection
+    private List<String> children = new ArrayList<String>();
 }
