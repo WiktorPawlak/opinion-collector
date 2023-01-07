5b37f4fe7781eb27655800301cbbd25a10a1a83f
Radek <236607@edu.p.lodz.pl>
2023-01-07 17:26:51 +0100

    add archive, changeRole

--- opinion-collector-bo/src/main/java/pl/io/opinioncollector/domain/client/ClientFacade.java
+++ opinion-collector-bo/src/main/java/pl/io/opinioncollector/domain/client/ClientFacade.java
@@ -2,10 +2,12 @@ package pl.io.opinioncollector.domain.client;
 
 import pl.io.opinioncollector.domain.client.model.Client;
 import pl.io.opinioncollector.domain.client.model.ClientDetails;
+import pl.io.opinioncollector.domain.client.model.ClientRole;
 import pl.io.opinioncollector.domain.dto.RegistrationDto;
 import pl.io.opinioncollector.domain.dto.SignInDto;
 import pl.io.opinioncollector.domain.client.model.ClientId;
 import java.util.List;
+import java.util.Optional;
 
 public interface ClientFacade {
     ClientId register(RegistrationDto registrationForm);
@@ -15,4 +17,8 @@ public interface ClientFacade {
     void changePass(String userName, String hashedPass);
     Client getClient(String username);
     List<Client> getAllClients();
+    void archive(String userName);
+    void changeRole(String userName, ClientRole role);
+    List<Client> getAllArchivedClients();
+    List<Client> getAllActiveClients();
 }
