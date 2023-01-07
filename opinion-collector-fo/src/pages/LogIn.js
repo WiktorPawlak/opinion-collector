d8c05a13d5e6c521438b5ce975b8f574f3446069
Weronika Tutkaj <nikat2001@gmail.com>
2023-01-07 17:46:43 +0100

    Login Page changes

--- opinion-collector-fo/src/pages/LogIn.js
+++ opinion-collector-fo/src/pages/LogIn.js
@@ -1,80 +1,33 @@
 import React, { useState } from "react";
+import CopyrightFooter from "../common/layouts/components/CopyrightFooter/CopyrightFooter";
 import "./LogIn.css";
 
-function getBase64(file) {
-  var reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onload = function () {
-    console.log(reader.result);
-  };
-  reader.onerror = function (error) {
-    console.log("Error: ", error);
-  };
-}
-
 function LogIn() {
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
-  const APP_KEY = "6d207e02198a847aa98d0a2a901485a5";
-  const changeHandler = (event) => {
-    //setSelectedFile(event.target.files[0]);
-    //setIsFilePicked(true);
-  };
-
-  const handleSubmission = (event) => {
-    const formData = new FormData();
-    formData.append("File", selectedFile);
-    var file = setSelectedFile(event.target.files[0]);
-    setIsFilePicked(true);
-    getBase64(file);
-
-    fetch(`https://freeimage.host/api/1/upload?key=${APP_KEY}`, {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log("Success:", result);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
   return (
     <div className="container">
       <div className="sign-up">
+        <div className="title">
+          Sign in to <span>NAME</span>🍕
+        </div>
         <div className="form-container">
           <form className="sign-up-form">
             <label>Email</label>
             <input type="text" />
-            <label>Username</label>
-            <input type="text" />
             <label>Password</label>
-            <input type="file" name="file" onChange={changeHandler} />
-            {isFilePicked ? (
-              <div>
-                <p>Filename: {selectedFile.name}</p>
-                <p>Filetype: {selectedFile.type}</p>
-                <p>Size in bytes: {selectedFile.size}</p>
-                <p>
-                  lastModifiedDate:{" "}
-                  {selectedFile.lastModifiedDate.toLocaleDateString()}
-                </p>
-              </div>
-            ) : (
-              <p>Select a file to show details</p>
-            )}
-            <button
-              onClick={handleSubmission}
-              className="search-btn"
-              type="submit"
-            >
-              Submit
+            <input type="text" />
+
+            <button className="signInButton" type="submit">
+              Sign In
             </button>
           </form>
         </div>
+        <a href="/sign-up">
+          <button className="joinUsButton">
+            New here? <span>Join us.</span>
+          </button>
+        </a>
       </div>
+      <CopyrightFooter />
     </div>
   );
 }
