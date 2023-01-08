add6bd5d82ac34bedabfc2ca73cd72b77134f916
o-trela <boskichlopaczyna@gmail.com>
2023-01-08 22:56:13 +0100

    register on front not working

--- opinion-collector-fo/src/api/authApi.js
+++ opinion-collector-fo/src/api/authApi.js
@@ -4,6 +4,10 @@ export function postLogin(body) {
   return post('/login', body);
 }
 
+export function postRegister(body) {
+  return post('/register', body);
+}
+
 export function apiGetSelf() {
   return get('/clients/self');
 }
