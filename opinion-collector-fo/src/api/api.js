d1c9b56d49c792ccb28ffae7e0a1eee7873259ff
o-trela <boskichlopaczyna@gmail.com>
2023-01-09 01:37:27 +0100

    Registration fixed

--- opinion-collector-fo/src/api/api.js
+++ opinion-collector-fo/src/api/api.js
@@ -42,6 +42,21 @@ export async function post(url, body, headers = defaultHeaders) {
   }
 }
 
+export async function postNoResponse(url, body, headers = defaultHeaders) {
+  const response = await fetch(`${BASE_URL}${url}`, {
+    headers,
+    credentials: 'include',
+    method: 'POST',
+    body: JSON.stringify(body)
+  });
+
+  if (response.created) {
+    return [response.status];
+  } else {
+    return [response.status];
+  }
+}
+
 export async function put(stringUrl, params) {
   const url = new URL(stringUrl, BASE_URL);
   console.log(params);
