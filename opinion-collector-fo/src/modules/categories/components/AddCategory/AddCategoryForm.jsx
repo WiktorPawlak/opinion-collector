02536ddffc0df882af1c9d6bf184c3e6b6ec919b
Krayan321 <krayan321@gmail.com>
2023-01-13 18:16:57 +0100

    Poprawiono dodawanie

--- opinion-collector-fo/src/modules/categories/components/AddCategory/AddCategoryForm.jsx
+++ opinion-collector-fo/src/modules/categories/components/AddCategory/AddCategoryForm.jsx
@@ -4,15 +4,29 @@ export const AddCategoryForm = ({
   handleSubmit,
   setCategoryId,
   setParentId,
-  setCategoryName
+  setCategoryName,
+  categories
 }) => (
   <form className="form-container">
-    <label>CategoryId</label>
-    <TextField onChange={(e) => setCategoryId(e.target.value)} />
-    <label>ParentId</label>
-    <TextField onChange={(e) => setParentId(e.target.value)} />
+    {/* <label>CategoryId</label>
+    <TextField onChange={(e) => setCategoryId(e.target.value)} /> */}
+    
     <label>Category Name</label>
-    <TextField onChange={(e) => {setCategoryName(e.target.value); console.log(`e.target.value: ${e.target.value}`)}} />    
+    <TextField onChange={(e) => {setCategoryName(e.target.value); console.log(`e.target.value: ${e.target.value}`)}} />   
+    <label>ParentId</label>
+    {/* <TextField onChange={(e) => setParentId(e.target.value)} /> */}
+    <Autocomplete
+      isOptionEqualToValue={(option, value) =>
+        option.categoryId === value.categoryId
+      }
+      getOptionLabel={(option) => option.categoryPath}
+      options={categories}
+      onChange={(_, value) => {
+        setParentId(value.categoryId);
+      }}
+      sx={{ width: '80%' }}
+      renderInput={(params) => <TextField {...params} label="Kategoria" />}
+    /> 
     <Button
       sx={{ width: '40' }}
       variant="contained"
